4ea50b2bfe9d5378ca8a7b4eba6ec07a7f4ad325
Knerk <keneerk@gmail.com>
2017-02-10 17:13:46 +0100

    Netwotk test

--- apps/elevator_network/lib/elevator_network.ex
+++ apps/elevator_network/lib/elevator_network.ex
@@ -1,3 +1,26 @@
 defmodule ENetwork do
- 
+ 	require Logger
+
+ 	def accept(port) do
+ 		{:ok, socket} = :gen_udp.open(port, [:binary, active: false, reuseaddr: true])
+ 		Logger.info "Accepting connections on port #{port}"
+ 		serve(socket)
+ 	end
+
+
+ 	defp serve(socket) do
+ 	:gen_udp.send(socket, {129, 241, 187, 255}, 30000, "hei")
+ 	socket |> read_line() |> write_line()
+ 	sleep(2000)
+ 	serve(socket)
+ 	end
+
+ 	defp read_line(socket) do
+ 		{:ok, data} = :gen_udp.rec(socket, 0)
+ 		data
+ 	end
+
+ 	defp write_line(line) do
+ 		IO.puts(line)
+ 	end
 end
--- apps/elevator_network/lib/elevator_network/application.ex
+++ apps/elevator_network/lib/elevator_network/application.ex
@@ -6,12 +6,14 @@ defmodule ENetwork.Application do
   use Application
 
   def start(_type, _args) do
-    import Supervisor.Spec, warn: false
+    import Supervisor.Spec
 
     # Define workers and child supervisors to be supervised
     children = [
       # Starts a worker by calling: ENetwork.Worker.start_link(arg1, arg2, arg3)
       # worker(ENetwork.Worker, [arg1, arg2, arg3]),
+      supervisor(Task.Supervisor, [[name: ENetwork.TaskSupervisor]]),
+      worker(Task, [ENetwork, :accept, [30000]])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
